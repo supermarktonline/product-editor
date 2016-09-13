dc09633748f866e045c09efe97c2b9971f05edd9
David Stöckl <david.stoeckl@inetcompany.com>
2016-09-13 15:43:19 +0200

    feat. update scripts for all gs1 categories, taggroups and tags for english and german

---
+++ importscripts/import-update-definition_etc-en.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: david
+ * Date: 9/9/16
+ * Time: 6:26 PM
+ */
+
+require('../config-local.php');
+require('../dal/db.php');
+
+$dbobj = new DB();
+$db = $dbobj->getDB();
+
+/* Note: We have line breaks within the definition of the csv therefore a simple explode wont work */
+$file = new SplFileObject('../../temp/gpc.en.csv');
+$rows = array();
+while (!$file->eof()) {
+    array_push($rows,$file->fgetcsv());
+}
+
+if(count($rows)<2) {
+    echo "Error finding or parsing CSV."; die;
+}
+
+$stmt_segment = $db->prepare("UPDATE category SET segment_description_en=:segment_description_en WHERE segment_code=:segment_code");
+$stmt_family = $db->prepare("UPDATE category SET family_description_en=:family_description_en WHERE family_code=:family_code");
+$stmt_class = $db->prepare("UPDATE category SET class_description_en=:class_description_en WHERE class_code=:class_code");
+$stmt_brick = $db->prepare("UPDATE category SET brick_description_en=:brick_description_en,brick_definition_en=:brick_definition_en WHERE brick_code=:brick_code");
+$stmt_attribute = $db->prepare("UPDATE taggroup SET muid=:muid,definition_en=:definition_en WHERE gs1_attribute_type_code=:gs1_attribute_type_code");
+$stmt_value = $db->prepare("UPDATE tag SET muid=:muid,definition_en=:definition_en WHERE gs1_attribute_value_code=:gs1_attribute_value_code");
+
+$in_segment = $db->prepare("INSERT INTO category SET segment_description_en=:segment_description_en WHERE segment_code=:segment_code");
+$in_family = $db->prepare("INSERT INTO category SET family_description_en=:family_description_en WHERE family_code=:family_code");
+$in_class = $db->prepare("INSERT INTO category SET class_description_en=:class_description_en WHERE class_code=:class_code");
+$in_brick = $db->prepare("INSERT INTO category SET brick_description_en=:brick_description_en,brick_definition_en=:brick_definition_en WHERE brick_code=:brick_code");
+$in_attribute = $db->prepare("INSERT INTO taggroup SET muid=:muid,definition_en=:definition_en WHERE gs1_attribute_type_code=:gs1_attribute_type_code");
+$in_value = $db->prepare("INSERT INTO tag SET muid=:muid,definition_en=:definition_en WHERE gs1_attribute_value_code=:gs1_attribute_value_code");
+
+$waroth=0;
+$warseg=0;
+$warfam=0;
+$warcla=0;
+$warbri=0;
+$waratt=0;
+$warval=0;
+
+foreach($rows as $row) {
+
+    $gs1id = @trim($row[0]);
+    $type = @trim($row[1]);
+    $name = @trim($row[2]);
+    $definition = @trim($row[3]);
+    $children = @trim($row[4]);
+
+    switch ($type):
+        case "segment": {
+            $stmt_segment->bindValue(":segment_description_en",$name);
+            $stmt_segment->bindValue(":segment_code",$gs1id);
+
+            $stmt_segment->execute();
+
+            if($stmt_segment->rowCount()<1) {
+                echo "Warning: No rows affected in update statement for segment: ".print_r($row,true).'<br/>';
+                $warseg++;
+            }
+            break;
+        }
+        case "family": {
+
+            $stmt_family->bindValue(":family_description_en",$name);
+            $stmt_family->bindValue(":family_code",$gs1id);
+
+            $stmt_family->execute();
+
+            if($stmt_family->rowCount()<1) {
+                echo "Warning: No rows affected in update statement for family: ".print_r($row,true).'<br/>';
+                $warfam++;
+            }
+            break;
+        }
+        case "class": {
+
+            $stmt_class->bindValue(":class_description_en",$name);
+            $stmt_class->bindValue(":class_code",$gs1id);
+
+            $stmt_class->execute();
+
+            if($stmt_class->rowCount()<1) {
+                echo "Warning: No rows affected in update statement for class: ".print_r($row,true).'<br/>';
+                $warcla++;
+            }
+            break;
+        }
+        case "brick": {
+
+            $stmt_brick->bindValue(":brick_description_en",$name);
+            $stmt_brick->bindValue(":brick_code",$gs1id);
+            $stmt_brick->bindValue(":brick_definition_en",$definition);
+
+            $stmt_brick->execute();
+
+            if($stmt_brick->rowCount()<1) {
+                echo "Warning: No rows affected in update statement for brick: ".print_r($row,true).'<br/>';
+                $warbri++;
+            }
+            break;
+        }
+        case "attribute": {
+
+            $stmt_attribute->bindValue(":muid",$name);
+            $stmt_attribute->bindValue(":definition_en",$definition);
+            $stmt_attribute->bindValue(":gs1_attribute_type_code",$gs1id);
+
+            $stmt_attribute->execute();
+
+            if($stmt_attribute->rowCount()<1) {
+                echo "Warning: No rows affected in update statement for attribute (tag group): ".print_r($row,true).'<br/>';
+                $waratt++;
+            }
+            break;
+        }
+        case "value": {
+
+            $stmt_value->bindValue(":muid",$name);
+            $stmt_value->bindValue(":definition_en",$definition);
+            $stmt_value->bindValue(":gs1_attribute_value_code",$gs1id);
+
+            $stmt_value->execute();
+
+            if($stmt_value->rowCount()<1) {
+                echo "Warning: No rows affected in update statement for value (tag): ".print_r($row,true).'<br/>';
+                $warval++;
+            }
+            break;
+        }
+        default: {
+            echo "Row not imported (invalid type): ".print_r($row,true).'<br/>';
+            $waroth++;
+            break;
+        }
+    endswitch;
+}
+
+echo "Segement failures: ".$warseg.'<br/>';
+echo "Family failures: ".$warfam.'<br/>';
+echo "Class failures: ".$warcla.'<br/>';
+echo "Brick failures: ".$warbri.'<br/>';
+echo "Attribute failures: ".$waratt.'<br/>';
+echo "Value failures: ".$warval.'<br/>';
+echo "Invalid row failures: ".$waroth.'<br/>';
+
+echo "Total failures: ".($warseg+$warfam+$warcla+$warbri+$waratt+$warval+$waroth).'<br/>';
+echo "Script finished.";
